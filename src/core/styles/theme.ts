ace4e88be2c5119c771ba76fb1c5de93e73fdb32
SOOEUN JEONG <soo@amuz.co.kr>
2025-07-21 12:13:56 +0900

    Text(marquee) 컴포넌트 추가

--- src/core/styles/theme.ts
+++ src/core/styles/theme.ts
@@ -1,19 +1,129 @@
-interface Theme {
-  size: {
-    gnbCollapsed: number;
-    gnbExpanded: number;
-  };
-  zIndex: {
-    gnb: number;
-  };
-}
+import { DefaultTheme } from 'styled-components/dist/types';
 
-export const theme: Theme = {
+export const theme: DefaultTheme = {
   size: {
     gnbCollapsed: 130,
     gnbExpanded: 400,
   },
   zIndex: {
     gnb: 1,
+  },
+  colors: {
+    keyColor: {
+      main: '#00FFCC',
+      item: '#00A584',
+      new: '#008A83',
+      mid: '#008F88',
+      deep: '#003633',
+    },
+    point: {
+      badge: '#FF5454',
+      hot: '#BF4658',
+    },
+    deactive: {
+      normal: 'rgba(230, 230, 230, 0.3)',
+      focused: 'rgba(76, 80, 89, 0.4)',
+    },
+    text: {
+      primary: '#E6E6E6',
+      primaryVari: '#2E3239',
+      tertiary: '#C7C8CC',
+      secondary: '#ABAEB3',
+      icon: '#8D9298',
+      secondaryVari: '#858B92',
+      focused: '#4C5059',
+    },
+    background: {
+      imagery: '#4B5157',
+      popup: '#373A41',
+      imageryVari: '#1F2226',
+      black: '#000000',
+    },
+  },
+  textStyle: {
+    headerHugeSb: {
+      fontSize: '72px',
+      fontWeight: 600,
+    },
+    headerXlSb: {
+      fontSize: '54px',
+      fontWeight: 600,
+    },
+    headerLgSb: {
+      fontSize: '45px',
+      fontWeight: 600,
+    },
+    headerMdSb: {
+      fontSize: '36px',
+      fontWeight: 600,
+    },
+    headerMdRg: {
+      fontSize: '36px',
+      fontWeight: 400,
+    },
+    headerSmSb: {
+      fontSize: '33px',
+      fontWeight: 600,
+    },
+    headerSmRg: {
+      fontSize: '33px',
+      fontWeight: 400,
+    },
+    titleXlSb: {
+      fontSize: '36px',
+      fontWeight: 600,
+    },
+    titleXlRg: {
+      fontSize: '36px',
+      fontWeight: 400,
+    },
+    titleLgSb: {
+      fontSize: '33px',
+      fontWeight: 600,
+    },
+    titleLgRg: {
+      fontSize: '33px',
+      fontWeight: 400,
+    },
+    titleMdSb: {
+      fontSize: '30px',
+      fontWeight: 600,
+    },
+    titleMdRg: {
+      fontSize: '30px',
+      fontWeight: 400,
+    },
+    titleSmSb: {
+      fontSize: '27px',
+      fontWeight: 600,
+    },
+    titleSmRg: {
+      fontSize: '27px',
+      fontWeight: 400,
+    },
+    titleTinySb: {
+      fontSize: '24px',
+      fontWeight: 600,
+    },
+    titleTinyRg: {
+      fontSize: '24px',
+      fontWeight: 400,
+    },
+    bodyMdRg: {
+      fontSize: '30px',
+      fontWeight: 400,
+    },
+    bodySmSb: {
+      fontSize: '24px',
+      fontWeight: 600,
+    },
+    bodyTinySb: {
+      fontSize: '21px',
+      fontWeight: 600,
+    },
+    bodyTinyRg: {
+      fontSize: '21px',
+      fontWeight: 400,
+    },
   },
 };
--- src/types/enact.d.ts
+++ src/types/enact.d.ts
@@ -35,5 +35,123 @@ declare module 'styled-components' {
     zIndex: {
       gnb: number;
     };
+    colors: {
+      keyColor: {
+        main: string;
+        item: string;
+        new: string;
+        mid: string;
+        deep: string;
+      };
+      point: {
+        badge: string;
+        hot: string;
+      };
+      deactive: {
+        normal: string;
+        focused: string;
+      };
+      text: {
+        primary: string;
+        primaryVari: string;
+        tertiary: string;
+        secondary: string;
+        secondaryVari: string;
+        icon: string;
+        focused: string;
+      };
+      background: {
+        imagery: string;
+        imageryVari: string;
+        popup: string;
+        black: string;
+      };
+    };
+    textStyle: {
+      headerHugeSb: {
+        fontSize: string;
+        fontWeight: number;
+      };
+      headerXlSb: {
+        fontSize: string;
+        fontWeight: number;
+      };
+      headerLgSb: {
+        fontSize: string;
+        fontWeight: number;
+      };
+      headerMdSb: {
+        fontSize: string;
+        fontWeight: number;
+      };
+      headerMdRg: {
+        fontSize: string;
+        fontWeight: number;
+      };
+      headerSmSb: {
+        fontSize: string;
+        fontWeight: number;
+      };
+      headerSmRg: {
+        fontSize: string;
+        fontWeight: number;
+      };
+      titleXlSb: {
+        fontSize: string;
+        fontWeight: number;
+      };
+      titleXlRg: {
+        fontSize: string;
+        fontWeight: number;
+      };
+      titleLgSb: {
+        fontSize: string;
+        fontWeight: number;
+      };
+      titleLgRg: {
+        fontSize: string;
+        fontWeight: number;
+      };
+      titleMdSb: {
+        fontSize: string;
+        fontWeight: number;
+      };
+      titleMdRg: {
+        fontSize: string;
+        fontWeight: number;
+      };
+      titleSmSb: {
+        fontSize: string;
+        fontWeight: number;
+      };
+      titleSmRg: {
+        fontSize: string;
+        fontWeight: number;
+      };
+      titleTinySb: {
+        fontSize: string;
+        fontWeight: number;
+      };
+      titleTinyRg: {
+        fontSize: string;
+        fontWeight: number;
+      };
+      bodyMdRg: {
+        fontSize: string;
+        fontWeight: number;
+      };
+      bodySmSb: {
+        fontSize: string;
+        fontWeight: number;
+      };
+      bodyTinySb: {
+        fontSize: string;
+        fontWeight: number;
+      };
+      bodyTinyRg: {
+        fontSize: string;
+        fontWeight: number;
+      };
+    };
   }
 }
